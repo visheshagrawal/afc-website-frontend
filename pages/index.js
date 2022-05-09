a03b7f624a7c517dba68de781f10bcfb033996c0
unknown <vishesh18420@iiitd.ac.in>
2022-05-10 03:42:01 +0530

    Added emotion fetch and post

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,64 @@
-import { homepageData } from '@/lib/data'
+import { useEffect } from 'react'
+import { homepageData, obtainCamera } from '@/lib/data'
+
+function handleError(error) {
+  console.log(
+    'navigator.MediaDevices.getUserMedia error: ',
+    error.message,
+    error.name
+  )
+}
 
 const Home = ({ homepageData }) => {
-  return <>{'nothing'}</>
+
+  useEffect(() => {
+    var video = document.getElementById('video')
+    let drawInterval
+    function handleSuccess(stream) {
+      window.stream = stream
+      video.srcObject = stream
+      video.onloadedmetadata = function (e) {
+        video.play()
+      }
+      drawInterval = setInterval(() => {
+        draw(stream)
+      }, 1)
+    }
+    obtainCamera(handleSuccess, handleError)
+    return () => {
+      clearInterval(drawInterval)
+    }
+  }, [])
+
+  const draw = (stream) => {
+    var video = document.getElementById('video')
+    const canvas = (window.canvas = document.querySelector('canvas'))
+    canvas.height = Math.max(document.getElementById('contentDiv').clientHeight, document.getElementById('contentDiv').offsetHeight)
+    canvas.width = canvas.height * (stream.getVideoTracks()[0].getSettings().width / stream.getVideoTracks()[0].getSettings().height)
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
+    let image_data_url = canvas.toDataURL('image/jpeg')
+    console.log(image_data_url)
+  }
+
+  return <>
+    <div
+      id="contentDiv"
+      className="h-screen w-screen z-10 overflow-hidden flex flex-col items-center justify-center"
+    >
+    </div>
+    <img
+      src=""
+      id="canvasImg"
+      className="w-screen h-screen object-cover hidden"
+    />
+    <canvas id="canvas" className='hidden'></canvas>
+    <video
+      id="video"
+      muted={true}
+      autoPlay={true}
+      className="hidden z-0"
+    ></video>
+  </>
 }
 
 export default Home
--- pages/product/[name].js
+++ pages/product/[name].js
@@ -1,12 +1,85 @@
 import Link from 'next/link'
-import { useState } from 'react'
-import { searchAllData } from '@/lib/data'
+import { useState, useEffect } from 'react'
 import { StarIcon } from '@heroicons/react/solid'
+import { searchAllData, homepageData, obtainCamera } from '@/lib/data'
 import { HeartIcon, StarIcon as StarIconOutline } from '@heroicons/react/outline'
 
-const Product = ({ data }) => {
+function handleError(error) {
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name)
+}
+
+const emojiMap = {
+  'angry': '😠',
+  'disgust': '🤢',
+  'fear': '😨',
+  'happy': '😄',
+  'neutral': '😐',
+  'sad': '😥',
+  'surprise': '😮'
+}
+
+const Product = ({ data, slug }) => {
+  const [emotionsData, setEmotionsData] = useState({})
+  const [sumEmotionsData, setSumEmotionsData] = useState(0)
   const [selectedImage, setSelectedImage] = useState(data.images[0].url)
-  const emotions = { angry: 5, disgust: 0, fear: 0, happy: 38, sad: 0, surprise: 0, neutral: 257 }
+
+  const draw = (stream) => {
+    var video = document.getElementById('video')
+    const canvas = (window.canvas = document.querySelector('canvas'))
+    canvas.height = Math.max(document.getElementById('contentDiv').clientHeight, document.getElementById('contentDiv').offsetHeight)
+    canvas.width = canvas.height * (stream.getVideoTracks()[0].getSettings().width / stream.getVideoTracks()[0].getSettings().height)
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
+    let image_data_url = canvas.toDataURL('image/jpeg')
+    fetch('http://localhost:5000/analyze', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ product_slug: slug, img: [image_data_url] })
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+      })
+  }
+
+  useEffect(() => {
+    var video = document.getElementById('video')
+    let drawInterval
+    function handleSuccess(stream) {
+      window.stream = stream
+      video.srcObject = stream
+      video.onloadedmetadata = function (e) {
+        video.play()
+      }
+      drawInterval = setInterval(() => {
+        draw(stream)
+      }, 1000)
+    }
+    obtainCamera(handleSuccess, handleError)
+    fetch('http://localhost:5000/get_emotions', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ product_slug: slug })
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (Object.keys(res).length < 1) {
+          let store = { "angry": 0, "disgust": 0, "fear": 0, "happy": 0, "sad": 0, "surprise": 0, "neutral": 0 }
+          setEmotionsData(store)
+          setSumEmotionsData(Object.keys(store).map((i) => res[i]).reduce((a, b) => a + b, 0))
+        }
+        else {
+          setEmotionsData(res)
+          setSumEmotionsData(Object.keys(res).map((i) => res[i]).reduce((a, b) => a + b, 0))
+        }
+      })
+    return () => {
+      clearInterval(drawInterval)
+    }
+  }, [])
 
   return (
     <div className="w-full flex flex-col items-start">
@@ -36,6 +109,10 @@ const Product = ({ data }) => {
         <div className="w-[35%] flex flex-col items-start px-10">
           <h1 className="font-bold text-3xl">{data.name}</h1>
           <h2 dangerouslySetInnerHTML={{ __html: data.description }} className="mt-5 font-light text-md"></h2>
+          <h1 className="mt-5 font-medium text-xl">{'Emotional Product Review Scale'}</h1>
+          <div className='mt-5 grid grid-cols-3 gap-3 text-center'>
+            {Object.keys(emotionsData).filter((i) => i !== 'fear').map((i) => <div key={i} className='flex flex-col items-center border rounded p-2'><span className='text-xl'>{emojiMap[i]}</span><span className='mt-1 text-lg font-medium'>{`${sumEmotionsData > 0 ? Math.round(10000 * (emotionsData[i] / sumEmotionsData)) / 100 : '--'}%`}</span><span className='text-gray-800 font-light'>{`${i.substring(0, 1).toUpperCase()}${i.substring(1)}`}</span></div>)}
+          </div>
           <div className="mt-10 w-full flex flex-row justify-between">
             <div className="flex flex-row items-center space-x-1">
               <StarIcon className="text-gray-700 w-[20px] h-[20px]" />
@@ -69,6 +146,10 @@ const Product = ({ data }) => {
           ))}
         </div>
       </div>
+      <canvas id="canvas" className='hidden'></canvas>
+      <video id="video" muted={true} autoPlay={true} className="hidden z-0"></video>
+      <img src="" id="canvasImg" className="w-screen h-screen object-cover hidden" />
+      <div id="contentDiv" className="h-screen w-screen z-10 overflow-hidden flex flex-col items-center justify-center"></div>
     </div>
   )
 }
